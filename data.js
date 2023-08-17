634f4f6ddcb158f2ddc65c65b6d7b95f96a350d0
ggors <ggorski@consoneo.com>
2023-08-17 15:38:43 +0200

    🎉 Init project with HTML & data files

---
+++ data.js
@@ -0,0 +1,6 @@
+var _data_from_backend = [
+    { "name": "Serveurs Bordeaux", "problem": "", "owner": "Pierre Richard", "urls": "3" },
+    { "name": "Serveurs Mérignac", "problem": "3", "owner": "Albert Dedieux", "urls": "0" },
+    { "name": "Serveurs Arcachon", "problem": "1", "owner": "Babette Jimenez", "urls": "2" },
+    { "name": "Serveurs Langon", "problem": "", "owner": "Pierre Richard", "urls": "1" }
+];
